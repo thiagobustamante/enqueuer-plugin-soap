507acd1b7d65fc4fdd0bac4bb82148a4bfe691c4
Thiago Bustamante <tbustaman@thoughtworks.com>
2019-06-15 09:57:09 -0300

    Fixed subscriptions

--- src/soap-subscription.ts
+++ src/soap-subscription.ts
@@ -1,12 +1,13 @@
 'use strict';
 
 import debug from 'debug';
-import { MainInstance, Subscription, SubscriptionModel, SubscriptionProtocol } from 'enqueuer-plugins-template';
+import { MainInstance, PublisherModel, Subscription, SubscriptionModel, SubscriptionProtocol } from 'enqueuer-plugins-template';
 import { HttpContainerPool } from 'enqueuer/js/pools/http-container-pool';
+import * as fs from 'fs';
 import * as _ from 'lodash';
 import * as soap from 'soap';
 import { SoapConfig } from './soap-config';
-// import { SoapPublisher } from './soap-publisher';
+import { SoapPublisher } from './soap-publisher';
 
 
 interface Message {
@@ -15,7 +16,7 @@ interface Message {
 }
 
 export class SoapSubscription extends Subscription {
-    // private readonly proxy: boolean;
+    private readonly proxy: boolean;
     private sendResults?: any;
     private secureServer: boolean;
     private httpServer: any;
@@ -30,15 +31,17 @@ export class SoapSubscription extends Subscription {
         this.soap = this.soap || {};
         this.type = this.type.toLowerCase();
         this.secureServer = this.isSecureServer();
-        // this.proxy = this.isProxyServer();
+        this.proxy = this.isProxyServer();
     }
 
     public async subscribe(): Promise<void> {
         try {
             this.httpServer = await HttpContainerPool.getApp(this.port, this.secureServer, this.credentials);
             this.httpServer.use((req: any, res: any, next: any) => {
+                this.debugger(`Server hit URL: %o`, req.url);
                 this.debugger(`Server hit with headers: %o`, req.headers);
-                this.debugger(`Server hit with body: %o`, req);
+                this.debugger(`Server hit with body: %o`, req.rawBody);
+                req.body = req.rawBody;
                 next();
             });
             this.debugger(`HTTP server listenning for soap requests on port: %d`, this.port);
@@ -54,6 +57,7 @@ export class SoapSubscription extends Subscription {
 
     public async receiveMessage(): Promise<any> {
         this.debugger(`Creating server for the given wsdl: %j`, this.soap.wsdl);
+        const wsdl = fs.readFileSync(this.soap.wsdl, 'utf8');
         return await Promise.resolve(new Promise((resolve, reject) => {
             const soapServer = soap.listen(this.httpServer, {
                 callback: (err, server) => {
@@ -61,7 +65,8 @@ export class SoapSubscription extends Subscription {
                 },
                 path: this.path || '/',
                 services: this.createServiceHandler(resolve, reject),
-                xml: this.soap.wsdl
+                uri: this.soap.wsdl,
+                xml: wsdl
             });
             if (this.headers) {
                 soapServer.addSoapHeader(() => {
@@ -84,40 +89,38 @@ export class SoapSubscription extends Subscription {
 
     private createServiceHandler(messageReceived: (message: any) => void, errorReceived: (error: any) => void) {
         this.debugger('Creating service handler for service [%s] port[%s] operation[%s].', this.soap.service, this.soap.port, this.soap.operation);
-        const serviceHandler = _.set({}, `${this.soap.service}.${this.soap.port}.${this.soap.operation}`,
-            (args: any) => {// headers: any
-                this.debugger(`${this.type}:${this.port} got hit (${this.soap.service}.${this.soap.port}.${this.soap.operation}) with args: %J`, args);
-                const message = this.createMessageReceivedStructure(args, {});
-                messageReceived(message);
-                return message;
-                // return new Promise<any>((resolve, reject) => {
-                //     try {
-                //         const message = this.createMessageReceivedStructure(args, headers);
-                //         this.sendResults = resolve;
-                //         if (this.proxy) {
-                //             this.callThroughProxy(message, messageReceived, errorReceived);
-                //         } else {
-                //             messageReceived(message);
-                //         }
-                //     } catch (error) {
-                //         errorReceived(error);
-                //     }
-                // });
+        const handler = (args: any, headers: any) => {
+            return new Promise<any>((resolve, reject) => {
+                try {
+                    const message = this.createMessageReceivedStructure(args, headers);
+                    this.sendResults = resolve;
+                    if (this.proxy) {
+                        this.callThroughProxy(message, messageReceived, errorReceived);
+                    } else {
+                        messageReceived(message);
+                    }
+                } catch (error) {
+                    errorReceived(error);
+                }
             });
+        };
+        const serviceHandler = _.set({}, `${this.soap.service}.${this.soap.port}.${this.soap.operation}`,
+            handler);
+        _.set(serviceHandler, `AirTicketLLSRQ`, handler);
         this.debugger('Service Handler created: %o.', serviceHandler);
 
         return serviceHandler;
     }
 
-    // private async callThroughProxy(message: Message, mesageReceived: (message: any) => void, errorReceived: (error: any) => void) {
-    //     try {
-    //         this.response = await this.redirectCall(message);
-    //         this.debugger(`${this.type}:${this.port} got redirection response: %J`, this.response);
-    //         mesageReceived(message);
-    //     } catch (err) {
-    //         errorReceived(err);
-    //     }
-    // }
+    private async callThroughProxy(message: Message, mesageReceived: (message: any) => void, errorReceived: (error: any) => void) {
+        try {
+            this.response = await this.redirectCall(message);
+            this.debugger(`${this.type}:${this.port} got redirection response: %J`, this.response);
+            mesageReceived(message);
+        } catch (err) {
+            errorReceived(err);
+        }
+    }
 
     private createMessageReceivedStructure(args: any, headers: any): Message {
         return {
@@ -126,38 +129,38 @@ export class SoapSubscription extends Subscription {
         };
     }
 
-    // private async redirectCall(message: Message): Promise<any> {
-    //     const config: PublisherModel = {
-    //         headers: message.headers,
-    //         name: this.name,
-    //         options: {
-    //             endpoint: this.redirect
-    //         },
-    //         payload: message.body,
-    //         service: this.soap.service,
-    //         soap: {
-    //             wsdl: this.soap.wsdl
-    //         },
-    //         target: this.target,
-    //         timeout: this.timeout,
-    //         type: this.type
-    //     };
+    private async redirectCall(message: Message): Promise<any> {
+        const config: PublisherModel = {
+            headers: message.headers,
+            name: this.name,
+            options: {
+                endpoint: this.redirect
+            },
+            payload: message.body,
+            service: this.soap.service,
+            soap: {
+                wsdl: this.soap.wsdl
+            },
+            target: this.target,
+            timeout: this.timeout,
+            type: this.type
+        };
 
-    //     const soapPublisher = new SoapPublisher(config);
-    //     this.debugger(`Redirecting call from ${this.path} (${this.soap.port}) to ${this.redirect}`);
-    //     return await soapPublisher.publish();
-    // }
+        const soapPublisher = new SoapPublisher(config);
+        this.debugger(`Redirecting call from ${this.path} (${this.soap.port}) to ${this.redirect}`);
+        return await soapPublisher.publish();
+    }
 
     private isSecureServer(): boolean {
         return (this.credentials ? true : false);
     }
 
-    // private isProxyServer(): boolean {
-    //     if (this.type) {
-    //         return this.type.indexOf('proxy') !== -1;
-    //     }
-    //     throw new Error(`Http server type is not known: ${this.type}`);
-    // }
+    private isProxyServer(): boolean {
+        if (this.type) {
+            return this.type.indexOf('proxy') !== -1;
+        }
+        throw new Error(`Http server type is not known: ${this.type}`);
+    }
 }
 
 export function entryPoint(mainInstance: MainInstance): void {
